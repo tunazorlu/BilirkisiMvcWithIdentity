d978db930d2e2ea7c754f53bf1eb126e3b1a8c55
Yaşar Tuna Zorlu <25878985+tunazorlu@users.noreply.github.com>
2024-05-02 17:03:11 +0300

    Login, Error ve Lockout süreçleri ele alındı

--- Controllers/HesapController.cs
+++ Controllers/HesapController.cs
@@ -14,19 +14,39 @@ namespace BilirkisiMvc.Controllers
         [HttpPost]
         public async Task<IActionResult> Giris(GirisModeli girisModeli)
         {
-            var user = await userManager.FindByEmailAsync(girisModeli.Eposta);
-
-            if (user != null)
+            if (ModelState.IsValid)
             {
-                var result = await signInManager.PasswordSignInAsync(user, girisModeli.Parola, girisModeli.BeniHatirla, false);
+                var user = await userManager.FindByEmailAsync(girisModeli.Eposta);
 
-                if (result.Succeeded)
+                if (user != null)
                 {
-                    return RedirectToAction("Index", "Home");
+                    await signInManager.SignOutAsync();
+                    var result = await signInManager.PasswordSignInAsync(user, girisModeli.Parola, girisModeli.BeniHatirla, true);
+
+                    if (result.Succeeded)
+                    {
+                        //Login işlemi başarılı iken kullanıcı girişi için Lockout süresi ve deneme sayısını sıfırlıyoruz.
+                        await userManager.ResetAccessFailedCountAsync(user);
+                        await userManager.SetLockoutEndDateAsync(user, null);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else if (result.IsLockedOut)
+                    {
+                        var lockoutDate = await userManager.GetLockoutEndDateAsync(user);
+                        var timeLeft = lockoutDate.Value.Subtract(DateTime.UtcNow);
+                        ModelState.AddModelError("", $"Hesabınız kilitlendi. {timeLeft.Minutes} dakika sonra tekrar deneyebilirsiniz.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Parolanız hatalı.");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Bu eposta ile kayıtlı bir kullanıcı bulunamadı.");
                 }
             }
-
-            return RedirectToAction("Giris");
+            return View("Giris");
         }
     }
 }
